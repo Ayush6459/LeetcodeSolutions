d5f8d187e91cec0a2d6594ade3d2279202627433
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-08-04 21:33:52 +0530

    Time: 48 ms (78.61%), Space: 13.7 MB (33.47%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,37 @@
+from collections import deque
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        d = {}
+        stack = deque()
+        
+        for i in nums2[::-1]:
+            # if the stack is empty
+            if not stack :
+                stack.append(i)
+                d[i]=-1
+            # if top is greater then curr element 
+            elif stack[-1]>i:
+                d[i] = stack[-1]
+                stack.append(i)
+            elif stack[-1]<= i:
+                while len(stack) and stack[-1]<=i:
+                    stack.pop()
+                if not stack:
+                    d[i]= -1
+                    stack.append(i)
+                else :
+                    d[i]=stack[-1]
+                    stack.append(i)
+        #print(d)
+        res = []
+        
+        for i in nums1:
+            if i in d:
+                res.append(d[i])
+        return res
+        
