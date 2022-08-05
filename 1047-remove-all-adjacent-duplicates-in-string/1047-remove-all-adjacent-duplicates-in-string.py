391e5036d9881a4849084157224c758e4de799ed
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-08-05 11:54:49 +0530

    Time: 127 ms (43.77%), Space: 14.8 MB (36.96%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -5,35 +5,13 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        stack = deque()
-        r = ''
-        n = len(s)-1
-        for i in range(len(s)):
-            # check if stack is empty 
-            # if empty just put the element in that 
-            if not stack :
-                stack.append(s[i])
+        stack = []
+        for ch in s:
+            if stack and ch == stack[-1]:
+                stack.pop()
             else:
-                # check if the current element is equal to the top of the stack or not
-                # if equal pop the element till the stack top not equal to the curr elemnet or
-                # stack become empty
-                # and if stack top not equal to the top put the curr elemnet in stack
-                
-                if stack[-1]!=s[i]:
-                    stack.append(s[i])
-                else:
-                    while stack and stack[-1]==s[i]:
-                        stack.pop()
-                    if i==n and not stack:
-                        stack.append(s[i])
-        
-        if len(stack)==1 and len(s)%2==0:
-            return ''
-            
-        
-        while stack :
-            r+=stack.pop()
-        return r[::-1]
+                stack.append(ch)
+        return "".join(stack)
             
                 
                     
