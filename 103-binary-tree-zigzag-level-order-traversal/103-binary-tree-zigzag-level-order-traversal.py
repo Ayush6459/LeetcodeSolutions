a646cb144d5ec6599cfffc04ac4f24bbbb40272a
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-08-18 19:32:52 +0530

    Time: 54 ms (42.70%), Space: 14.2 MB (58.42%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        s1 = deque() 
+        s2 = deque()
+        # check if root is empty
+        if not root:
+            return []
+        """
+            idea : 
+                first put the root in first stack s1
+                now traverse till any of the stack is not empty
+                    while s1 is not empty 
+                        a. take out the node and store it in result
+                        b. push the children to in s2
+                    while s2 is not empty 
+                        a. take out the node and store it in result
+                        b. push the childrens to in s1
+        """
+        res = []
+        s1.append(root)
+        while s1 or s2:
+            if s1:
+                temp =[]
+                while s1:
+                    x = s1.pop()
+                    temp.append(x.val)
+                    if x.left: 
+                        s2.append(x.left)
+                    if x.right:
+                        s2.append(x.right)
+                res.append(temp)
+            if s2:
+                temp = []
+                while s2:
+                    x = s2.pop()
+                    temp.append(x.val)
+                    if x.right:
+                        s1.append(x.right)
+                    if x.left:
+                        s1.append(x.left)
+                res.append(temp)
+                
+        return res
