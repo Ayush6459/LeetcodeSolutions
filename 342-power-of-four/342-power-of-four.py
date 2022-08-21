ff69cd8125fba31e9ad5ffca1f523d87c6328a78
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-08-21 09:18:29 +0530

    Time: 36 ms (88.55%), Space: 13.8 MB (95.34%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,12 +1,3 @@
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n<=0:
-            return False
-        n = f'{n:b}'
-        x = n.count('1')
-        y = n.count('0')
-        if x>1:
-            return False
-        if x == 1 and y%2!=0:
-            return False
-        return True
+    def isPowerOfFour(self, num: int) -> bool:
+        return num>0 and num&(num-1)==0 and len("{0:b}".format(num))%2==1
