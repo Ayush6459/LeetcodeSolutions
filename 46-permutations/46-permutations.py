395f776f8590f2c50ebfc3ea628a68b91675b6f7
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-08-25 19:48:59 +0530

    Time: 56 ms (66.00%), Space: 14 MB (84.57%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,32 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        '''# method 1 
-        res = list()
-        map = dict.fromkeys(range(len(nums)),0)
-        self.generate(nums,[],res,map)
-        
-        return res
-    def generate(self,nums,temp,res,map):
-        if len(temp) == len(nums):
-            print(temp)
-            res+=[temp]
+        result = []
+        if (len(nums)==1):
+            return [nums[:]]
         for i in range(len(nums)):
-            if not map[i]:
-                map[i] = 1
-                temp.append(nums[i])
-                self.generate(nums,temp,res,map)
-                temp.pop()
-                map[i]=0
-        return res'''
-        visited = set()
-        res = []
-        self.backtracking(res,visited,[],nums)
-        return res
-    def backtracking(self,res,visited,subset,nums):
-        if len(subset) == len(nums):
-            res.append(subset)
-        for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.backtracking(res,visited,subset+[nums[i]],nums)
-                visited.remove(i)
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            for i in perms:
+                i.append(n)
+            result.extend(perms)
+            nums.append(n)
+        return result
