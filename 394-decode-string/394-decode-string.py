9f3f57b721618592f75dc763b8db02b864c0709a
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-09-01 21:29:16 +0530

    Time: 49 ms (43.50%), Space: 14 MB (20.23%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        i = 0
+        s1 = [] # stack 1
+        s2 = [] # stack 2
+
+        while i<len(s):
+            if s[i].isdigit():
+                temp = s[i]
+                i+=1
+                while s[i].isdigit():
+                    temp = temp+s[i]
+                    i+=1
+                s1.append(int(temp))
+                i-=1
+
+                #continue
+            if s[i].isalpha() or s[i] =='[':
+                s2.append(s[i])
+
+                #continue
+            if s[i] == ']':
+                temp = ''
+                while s2 and s2[-1]!='[':
+                    temp = s2.pop()+temp
+
+                s2.pop()
+                if s1:
+
+                    s2.append(temp*s1.pop())
+                else:
+                    s2.append(temp)
+
+            i+=1
+
+                #continue
+        temp = ''
+        while s2:
+            temp = s2.pop()+temp
+        return temp
