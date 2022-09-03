7bb75d62bd7ad5b5dd4858f45aaf5faa919a62ba
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-09-03 13:04:45 +0530

    Time: 75 ms (34.01%), Space: 14.2 MB (41.16%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,5 +1,32 @@
 class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        # efficient dfs solution
+        if N == 1:
+            return [i for i in range(10)]
+
+        # initialize the queue with candidates for the first level
+        queue = [digit for digit in range(1, 10)]
+
+        for level in range(N-1):
+            next_queue = []
+            for num in queue:
+                tail_digit = num % 10
+                # using set() to avoid duplicates when K == 0
+                next_digits = set([tail_digit + K, tail_digit - K])
+
+                for next_digit in next_digits:
+                    if 0 <= next_digit < 10:
+                        new_num = num * 10 + next_digit
+                        next_queue.append(new_num)
+            # start the next level
+            queue = next_queue
+
+        return queue
+        
+        
+        
+        '''
+        # my solution using dfs concept
         def generate(s,n,k,res):
             if len(s)==n:
                 res.append(int(s))
@@ -22,3 +49,5 @@ class Solution:
             s=str(int(s)+1)
             
         return result
+        '''
+        
