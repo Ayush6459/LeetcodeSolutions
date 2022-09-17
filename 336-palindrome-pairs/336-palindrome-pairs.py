5b046dbd629ef11898be6974955eabee46d136d6
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-09-17 19:51:41 +0530

    Time: 6972 ms (19.54%), Space: 26.2 MB (93.16%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        def is_palindrome(check):
+            return check == check[::-1]
+
+        words = {word: i for i, word in enumerate(words)}
+        valid_pals = []
+        for word, k in words.items():
+            n = len(word)
+            for j in range(n+1):
+                pref = word[:j]
+                suf = word[j:]
+                if is_palindrome(pref):
+                    back = suf[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([words[back],  k])
+                if j != n and is_palindrome(suf):
+                    back = pref[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([k, words[back]])
+        return valid_pals
