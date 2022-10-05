b0bd745c98ee3a64249d03ed23c1c758c98a0d24
Ayush6459 <ayushranjan6459@gmail.com>
2022-10-05 11:19:36 +0530

    dfs solution for right view of binary tree

--- problem practiced/BinaryTree/rightviewofBT.py
+++ problem practiced/BinaryTree/rightviewofBT.py
@@ -19,4 +19,20 @@ def rightSideView(root):
 # solution using dfs 
 
 def dfsRightSideView(root):
-    pass
+    # The plan here is to dfs the tree, right-first
+    # (opposite of  the usual left-first method), and
+    # keeping track of the tree levels as we proceed. The
+    # first node we visit on each level is the right-side view
+    # node. We know it's the first because the level will be
+    # one greater than the length of the current answer list.
+    ans = []
+    def dfs(node=root,level=1):
+        if not node : return 
+        if len(ans)<level:
+            ans.append(node.val)
+        dfs(node.right,level+1)
+        dfs(node.left,level+1)
+        return 
+    dfs(root)
+    return ans 
+    
