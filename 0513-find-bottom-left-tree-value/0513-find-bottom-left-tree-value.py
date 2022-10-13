1c7c7df1f799ace2a7e89e0952aa329ace2d5774
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-10-13 23:01:18 +0530

    Time: 93 ms (31.82%), Space: 16.3 MB (66.43%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -6,22 +6,21 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        ans = 0
         
-        def bfs(root):
-            if root is None :return
-            q  = [root]
-            prev = []
-            while q:
-                
-                temp = []
-                for i in q:
-                    temp.extend([i.left,i.right])
-                prev = q[:]
-                q = [i for i in temp if i]
-                
-            return prev[0].val
-        return (bfs(root))
+        if root is None :return
+        q  = [root]
+        prev = []
+        while q:
+
+            temp = []
+            for i in q:
+                temp.extend([i.left,i.right])
+            prev = q[:]
+            q = [i for i in temp if i]
+
+        return prev[0].val
+        
+        
             
         
                 
