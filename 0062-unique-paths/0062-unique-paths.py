3834be721a8dd896051affefe32a80c90db9165e
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-10-17 10:09:10 +0530

    Time: 70 ms (13.38%), Space: 13.9 MB (32.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,7 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         # dp with memoization 
-        dpArr = [[-1 for _ in range(n)] for _ in range(m)]
+        #dpArr = [[-1 for _ in range(n)] for _ in range(m)]
         def usingMemoization(i,j):
             if i ==0 and j == 0:return 1
             if i<0 or j<0 : return 0
@@ -15,6 +15,7 @@ class Solution:
         
         #return  usingMemoization(m-1,n-1)
     
+        # using tabulation method
         def usingTabulation():
             dpArr[0][0] = 1
             for i in range(0,m):
@@ -30,5 +31,26 @@ class Solution:
                         dpArr[i][j] = up+left
                         
             return dpArr[m-1][n-1]
-        return usingTabulation() 
+        #return usingTabulation() 
+        
+        # tabulation with space optimisation 
+        def spaceOptimised(m,n):
+            prev = [0]*n
+            
+            for i in range(m):
+                curr = [0]*n
+                for j in range(n):
+                    if i == 0 and j == 0 : curr[j] = 1
+                    else:
+                        up = prev[j]
+                        left = curr[j-1]
+                        curr[j] = up+left
+                        
+                prev = curr
+                
+            return prev[n-1]
+        
+        return spaceOptimised(m,n)
+        
+        
         
