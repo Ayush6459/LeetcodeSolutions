e30bb1a3410ffd049e5dfa0214e5b3b950e82e31
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-10-26 11:59:36 +0530

    Time: 60 ms (44.52%), Space: 13.9 MB (19.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # using memoization technique
-        dp = [-1]*len(nums)
-        def memo(index):
-            if index==0:return nums[index]
-            if index<0: return 0
-            if dp[index]!= -1:
-                return dp[index]
-            pick = nums[index] + memo(index-2)
-            notPick = memo(index-1)
-            dp[index] = max(pick,notPick)
-            return dp[index]
-        return memo(len(nums)-1)
+        # using tabulation method 
+        dp = [nums[0]]
+        
+        for i in range(1,len(nums)):
+            pick = nums[i]
+            if i>1:
+                pick+=dp[i-2]
+            notPick = dp[i-1]
+            dp.append(max(pick,notPick))
+        return dp[-1]
+            
+            
         
