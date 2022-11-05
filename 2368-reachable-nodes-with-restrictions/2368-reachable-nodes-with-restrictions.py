7bd7be8256f25e66d1821d9cce772229fbf28390
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-11-05 19:25:59 +0530

    Time: 2479 ms (79.03%), Space: 72.4 MB (84.85%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
@@ -1,29 +1,32 @@
+class UnionFind:
+    def __init__(self, n):
+        self.root = list(range(n))
+        self.rank = [1] * n
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                root_x, root_y = root_y, root_x
+            self.rank[root_y] += self.rank[root_x]
+            self.root[root_x] = root_y
+    def getSize(self, x):
+        return self.rank[self.find(x)]
+
 class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        rest_set = set(restricted)
+        uf = UnionFind(n)
+
+        for a, b in edges:
+            if a not in rest_set and b not in rest_set:
+                uf.union(a, b)
         
-        def makegraph():
-            graph = [[] for _ in range(n)]
-            for u, v in edges:
-                graph[u].append(v)
-                graph[v].append(u)
-                
-            return graph
-        graph = makegraph()
+        return uf.getSize(0)
         
-        ans = 1
-        restricted = set(restricted)
-        visited = set()
-        visited.add(0) # add 0th node as a visited node 
-        q = [0] # put the 0th node in queue 
-        while q :
-            u = q.pop(0)
-            for v in graph[u]:
-                if v not in visited and v not in restricted :
-                    q.append(v)
-                    visited.add(v)
-                    ans+=1
-                    
-        return ans
                     
             
         
