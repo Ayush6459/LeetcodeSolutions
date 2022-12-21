969c4828810e7cd978d286d5599af754116374b2
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2022-12-21 20:42:38 +0530

    Time: 467 ms (21.85%), Space: 23.7 MB (91.87%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -13,17 +13,16 @@ So, for any even index i, we can compare nums[i] to nums[i+1].
 If they are equal, the unique element must occur somewhere after index i+1
 If they aren't equal, the unique element must occur somewhere before index i+1
 Using this knowledge, we can use binary search to find the unique element.
-
-We just have to make sure that our pivot index is always even, so we can use mid = 2 * ((lo + hi) // 4) instead of the usual mid = (lo + hi) // 2.
         
         '''
         
         l=0
         h=len(nums)-1
         while l<h:
-            m=2*((l+h)//4)
-            if nums[m]==nums[m+1]:
-                l=m+2
+            m=(l+h)//2
+            n = m+1 if m%2 == 0 else m-1 # here we are deciding if our mid index is odd or even if it is even then we will compare with next element after mid otherwise we will compair with elment before it  
+            if nums[m]==nums[n]:
+                l=m+1
             else:
                 h=m
         return nums[l] 
