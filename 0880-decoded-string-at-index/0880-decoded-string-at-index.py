b468df72002ce1f81221cc39314424b7320c671d
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2023-01-09 19:06:05 +0530

    Time: 35 ms (76.03%), Space: 13.8 MB (98.63%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def decodeAtIndex(self, S: str, K: int) -> str:
+        
+        size = 0
+        # Find size = length of decoded string
+        for c in S:
+            if c.isdigit():
+                size *= int(c)
+            else:
+                size += 1
+
+        for c in reversed(S):
+            K %= size
+            if K == 0 and c.isalpha():
+                return c
+
+            if c.isdigit():
+                size /= int(c)
+            else:
+                size -= 1
