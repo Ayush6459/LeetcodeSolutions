fef32d4a207de8cc836cb047561e9ea279ad7e73
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2023-01-15 20:28:45 +0530

    Time: 30 ms (89.03%), Space: 13.8 MB (62.62%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        rods = [set() for _ in range(10)]
+        for i in range(0,len(rings),2):
+            rods[int(rings[i+1])].add(rings[i])
+        count=0
+        for i in rods:
+            if len(i)==3:
+                count+=1
+        return count
