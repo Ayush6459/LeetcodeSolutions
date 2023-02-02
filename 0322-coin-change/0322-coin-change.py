3917ece35df94b80e0ad9c9b25ab5afa537a312c
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2023-02-02 09:34:22 +0530

    Time: 1791 ms (44.95%), Space: 43.5 MB (7.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        return self.memoization(coins,amount)
-    def memoization(self, coins: List[int], amount: int) -> int:
         dp = {}
         
-        def dfs(amount):
-            if amount in dp:
-                return dp[amount]
+        def helper(amount):
             if amount == 0:
                 return 0
-            
-            res = math.inf
+            if amount in dp:return dp[amount]
+            ans = sys.maxsize
             for coin in coins:
-                if (amount - coin) >= 0:
-                    res = min(res,1 + dfs(amount - coin))
-            dp[amount] = res
-            return res
-        
-        res = dfs(amount)
-        return res if res != math.inf else -1
+                if amount-coin>=0:
+                    subproblem = helper(amount-coin)
+                    if subproblem != -1:
+                        ans = min(ans,subproblem+1)
+                        
+                    
+            dp[amount] = ans if ans!=sys.maxsize else -1
+            return dp[amount]
+        return helper(amount)
+            
