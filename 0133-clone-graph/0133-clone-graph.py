86b9dc514be1c4fbd0b6758c246e05be04f48ff2
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2023-02-22 10:16:08 +0530

    Time: 66 ms (9.20%), Space: 14.4 MB (68.87%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,7 +10,7 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node : return 
         visited = {}
-        
+        '''
         def DFS(node:'Node') -> 'Node':
             clone = Node(node.val) # make a new tree node
             # add the current node in visited
@@ -21,6 +21,19 @@ class Solution:
                     clone.neighbors.append(visited[v.val])
                 else:
                     clone.neighbors.append(DFS(v))   
-            return clone
-        return DFS(node)
+            return clone'''
+        
+        def bfs(node):
+            q = deque([node])
+            clone = {node.val:Node(node.val)}
+            while q:
+                curr = q.popleft()
+                curr_clone = clone[curr.val]
+                for v in curr.neighbors:
+                    if v.val not in clone:
+                        clone[v.val]= Node(v.val)
+                        q.append(v)
+                    curr_clone.neighbors.append(clone[v.val])
+            return clone[node.val]
+        return bfs(node)
             
