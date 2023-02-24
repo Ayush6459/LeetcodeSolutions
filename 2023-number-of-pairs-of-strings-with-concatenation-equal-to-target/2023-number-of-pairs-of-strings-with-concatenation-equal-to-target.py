4c30f1a9143fdc16985df5ed6832e738077f238a
Ayush Ranjan <37879054+Ayush6459@users.noreply.github.com>
2023-02-24 21:30:27 +0530

    Time: 44 ms (79.88%), Space: 14 MB (60.52%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -1,13 +1,26 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                temp = nums[i]+nums[j]
-                temp2 = nums[j]+nums[i]
-                if temp == target:
-                    count+=1
-                if temp2 == target:
-                    count+=1
-        return count
+        '''
+        Idea:
+First, we store in a map the frequencies of the strings, so that we can find easily which strings we have and how many.
+Now, we iterate through the freq map.
+For every string:
+
+We check if it's a prefix of our target.
+If yes, first case is that the target is exactly twice the prefix. If so, we add frq*(frq-1) to res.
+The reason is that the number of combinations for a pattern with frequency n is n * (n-1).
+Otherwise we look in the map if we have the suffix, if so - we add the product of their frequencies to res.
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+        '''
+        ans = 0 
+        freq = collections.Counter(nums)
+        for k,v in freq.items():
+            if target.startswith(k): 
+                suffix = target[len(k):]
+                ans += v * freq[suffix]
+                if k == suffix: ans -= freq[suffix]
+        
+        return ans
             
